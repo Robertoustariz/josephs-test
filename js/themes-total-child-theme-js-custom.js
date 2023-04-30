2e2509016ba7047dd64863baec427d4f8337c4ab
Robertoustariz <58562884+Robertoustariz@users.noreply.github.com>
2023-04-30 00:11:33 -0400

    Subida de images, js e index

---
+++ js/themes-total-child-theme-js-custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function () {
+    if (jQuery('.merchant-info').is('div')) {
+        jQuery('.merchant-info .theme-button').click(function () {
+            return false;
+        });
+        jQuery('.merchant-info .theme-button').click(function () {
+            var classButton = jQuery(this).attr('href').replace('#', '');
+            var imgBlock = jQuery('.merchant-info .' + classButton);
+            if (!jQuery(this).is('current')) {
+                jQuery('.merchant-info .theme-button').removeClass('current');
+                jQuery(this).addClass('current');
+            }
+            if (!imgBlock.is('.current')) {
+                jQuery('.merchant-info .wpb_single_image').removeClass('current');
+                imgBlock.addClass('current');
+            }
+            return false;
+        });
+    }
+});
+
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
